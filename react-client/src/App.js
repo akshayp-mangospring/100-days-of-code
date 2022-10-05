1bdc270d95c3dedfceccd5b21247faa266cfefb6
akshayp-mangospring <akshayp@mangospring.com>
2022-10-05 10:00:18 +0530

    Enable CORS in Rails App - Integrate the first(root_route) API

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,6 +1,16 @@
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:3000', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+      .then((r) => r.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <h1>Sieg Heil!</h1>
   );
