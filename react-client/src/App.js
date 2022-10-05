0834d3b77591792774a485e9664db6d28729f702
akshayp-mangospring <akshayp@mangospring.com>
2022-10-05 10:33:40 +0530

    Add Simple CSS, popup with overlay component in React App

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,6 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import EntryForm from "./EntryForm";
 
 function App() {
+  const [foodItems, setFoodItems] = useState([]);
+  const [showEntry, setShowEntry] = useState(false);
+
   useEffect(() => {
     fetch('http://localhost:3000', {
       headers: {
@@ -8,11 +12,24 @@ function App() {
       },
     })
       .then((r) => r.json())
-      .then((data) => console.log(data));
+      .then((data) => setFoodItems(data));
   }, []);
 
   return (
-    <h1>Sieg Heil!</h1>
+    <>
+      <div className="d-flex justify-content-between align-items-center">
+        <h1>Food Entries</h1>
+        <button type="button" onClick={() => setShowEntry(!showEntry)}>New Entry</button>
+      </div>
+      {showEntry && <EntryForm setShowEntry={setShowEntry} />}
+      {
+        foodItems.length ? (
+          <ul>
+            <li></li>
+          </ul>
+        ) : <h1>No Food Items</h1>
+      }
+    </>
   );
 }
 
