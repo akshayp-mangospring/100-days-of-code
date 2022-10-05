83373f79341203b86c64a87d0ce1aab61b22918f
akshayp-mangospring <akshayp@mangospring.com>
2022-10-05 08:13:33 +0530

    Add validation for numbers in number fields

--- food_log/app/models/entry.rb
+++ food_log/app/models/entry.rb
@@ -1,3 +1,4 @@
 class Entry < ApplicationRecord
   validates :meal_type, :calories, :proteins, :carbohydrates, :fats, presence: true
+  validates :calories, :proteins, :carbohydrates, :fats, numericality: true
 end
