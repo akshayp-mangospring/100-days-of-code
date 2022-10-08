fb8fdea87b46450daf5bc3ad09243a610a69ff5b
hiteshp-mangospring <hiteshp@mangospring.com>
2022-10-08 19:17:09 +0530

    Add login API with password encryption - Add hashing of password for a user using bcrypt

--- food_log_api/app/controllers/auth_controller.rb
+++ food_log_api/app/controllers/auth_controller.rb
@@ -1,9 +1,19 @@
 class AuthController < ApplicationController
+  include BCrypt
+
   def login
+    @user = User.find_by_user_name(params[:user_name])
+
+    if @user.password == params[:password]
+      render json: { message: 'Authenticated' }
+    else
+      render json: { message: 'Unauthenticated' }
+    end
   end
 
   def signup
     @user = User.new(user_params)
+    @user.password = BCrypt::Password.create(params[:password])
 
     if @user.save
       render json: @user
